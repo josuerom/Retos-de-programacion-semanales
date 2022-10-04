8512eaf775fca7390fdb4625bbd388561183ee27
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-10-04 11:26:32 -0500

    Update AreaDeUnPoligono.java

--- Solutions/AreaDeUnPoligono.java
+++ Solutions/AreaDeUnPoligono.java
@@ -1,15 +1,56 @@
+import java.util.Scanner;
+
 public class AreaDeUnPoligono {
 
    public static void main(String[] args) {
-      
-   }
+      Scanner sc = new Scanner(System.in);
+      String pol = sc.next();
 
-   public static void cuadrado(float lado) {
-   }
+      Poligono poligono;
 
-   public static void rectangulo(float base, float altura) {
+      if (pol.equals("cuadrado")) {
+        float lado = sc.nextFloat();
+        poligono = new Poligono(lado);
+        poligono.cuadrado();
+      } else if (pol.equals("rectangulo")) {
+        float base = sc.nextFloat();
+        float altura = sc.nextFloat();
+        poligono = new Poligono(base, altura);
+        poligono.rectangulo();
+      } else if (pol.equals("triangulo")) {
+        float base = sc.nextFloat();
+        float altura = sc.nextFloat();
+        poligono = new Poligono(base, altura);
+        poligono.triangulo();
+      }
    }
+}
 
-   public static void triangulo(float base, float altura) {
-   }
+class Poligono {
+    private float lado1;
+    private float lado2;
+
+    public Poligono(float lado1) {
+        this.lado1 = lado1;
+    }
+
+    public Poligono(float lado1, float lado2) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+
+    public void cuadrado() {
+        float resultado = (lado1 * lado1);
+        System.out.println("Área del cuadrado: " + resultado);
+    }
+
+    public void rectangulo() {
+        float resultado = (lado1 * lado2);
+        System.out.println("Área del rectángulo: " + resultado);
+    }
+
+    public void triangulo() {
+        float resultado = (lado1 * lado2) / 2;
+        System.out.println("Área del triángulo: " + resultado);
+    }
 }
